d8f7181605e8ef84c42fb0e85e58c3ef9c533715
Jake <94998878+jakebark@users.noreply.github.com>
2026-02-13 16:37:48 +0000

    external users (#4)
    update to accept external users

--- local.tf
+++ local.tf
@@ -7,14 +7,18 @@ locals {
 
   groups_flatten = flatten([
     for group_name, group_details in local.groups_list : [
-      for user in group_details.users :
-      {
+      for user in group_details.users : {
         user  = user
         group = group_name
-      }
+      } if user != null && user != ""
     ]
   ])
 
   users_yaml = var.users != null ? file(var.users) : ""
   users_list = local.users_yaml != "" ? yamldecode(local.users_yaml) : {}
+
+  existing_usernames = var.users == null ? distinct([
+    for group in local.groups_flatten : group.user
+  ]) : []
 }
+
