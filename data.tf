f56189c819c8be1bf57c0163586e81890f8a3578
jakebark <jakebark@amazon.com>
2026-02-13 15:59:39 +0000

    conditional types fixc

--- data.tf
+++ data.tf
@@ -4,7 +4,7 @@
 data "aws_ssoadmin_instances" "this" {}
 
 data "aws_identitystore_user" "existing" {
-  for_each          = var.users == null ? toset(local.existing_usernames) : {}
+  for_each          = var.users == null ? toset(local.existing_usernames) : toset([])
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
   alternate_identifier {
