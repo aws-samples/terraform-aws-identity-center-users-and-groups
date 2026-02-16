1839dde50f1ca38a78ce417c747dd488200fd70e
jakebark <jakebark@amazon.com>
2026-02-16 20:26:29 +0000

    enable mixture of existing and new users in a group

--- data.tf
+++ data.tf
@@ -4,7 +4,7 @@
 data "aws_ssoadmin_instances" "this" {}
 
 data "aws_identitystore_user" "existing" {
-  for_each          = var.users == null ? toset(local.existing_usernames) : toset([])
+  for_each          = toset(local.existing_usernames)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
   alternate_identifier {
